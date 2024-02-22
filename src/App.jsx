480642b460f1771c583e3c1d59a7265fd549207e
JUDAS89 <lorena.peroza@outlook.com>
2024-02-21 21:25:33 -0300

    prueba 3

--- src/App.jsx
+++ src/App.jsx
@@ -6,18 +6,6 @@ function App() {
 
   return (
     <>
-<<<<<<< HEAD
-
-=======
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -29,7 +17,6 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more, Prueba lore
       </p>
->>>>>>> ba0fe81a72ba20967d5651ad461bede520dccd36
     </>
   )
 }
